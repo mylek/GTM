031175688dc1fbfdde5cc2848ff20d9c53b3ea5a
kgwozdz2 <kgwozdz2@unitygroup.com>
2023-01-12 07:02:41 +0100

    init

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Catalog/js/catalog-add-to-cart': {
+                'MylSoft_GTM/js/catalog-add-to-cart-mixin': true
+            }
+        }
+    }
+};
